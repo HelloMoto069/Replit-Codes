3d27ddc1cbe274901e53f44aae7f4666c5962992
ANURUDDH <hellomoto.1769@gmail.com>
2022-02-16 10:30:23 +0530

    29/07/2021

---
+++ Introduction-to-Javascript-IW/index.js
@@ -0,0 +1,48 @@
+
+var birthyear = 1993;
+var cllg = "dma3";
+var cllg2 = "gpb";
+var cllg3 = "kiot";
+
+console.log("Anuruddh");
+console.log("    ")
+console.log(birthyear);
+console.log("    ")
+console.log(cllg);
+console.log(cllg2);
+console.log(cllg3);
+console.log("    ")
+
+var owner = "Moto";
+var resname = "Hello Moto";
+var paneer = 180;
+var roti =10;
+var choclate= 15;
+var dal = 23;
+var mixveg = 20;
+var pizza = 100;
+
+
+console.log("restaurant name");
+console.log(resname);
+console.log("********************");
+console.log("owner of restaurant");
+console.log(owner);
+console.log("********************");
+console.log("paneer");
+console.log(paneer);
+console.log("=====");
+console.log("Pizza");
+console.log(pizza);
+console.log("=====");
+console.log("Roti");
+console.log(roti);
+console.log("=====");
+console.log("Dal");
+console.log(dal);
+console.log("=====");
+console.log("Mix-Veg");
+console.log(mixveg);
+console.log("=======");
+console.log("choclate");
+console.log(choclate);
---
+++ Introduction-to-Javascript-IW/practice.js
@@ -0,0 +1,9 @@
+var n = 4;
+  for (i=n ; i>0; i--) {
+    output = ""
+    for (j=n ; j>0; j--) {
+      output = output + i + " ";
+      
+    }
+    console.log(output)
+  }
