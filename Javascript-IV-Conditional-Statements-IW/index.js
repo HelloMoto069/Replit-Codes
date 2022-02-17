41b943a5c179e94b8ddf55f008219ddaf4b83ef7
ANURUDDH <hellomoto.1769@gmail.com>
2022-02-17 10:55:18 +0530

    02/08/2021

---
+++ Javascript-IV-Conditional-Statements-IW/index.js
@@ -0,0 +1,54 @@
+console.log("Question 1:")
+console.log(" ")
+var n = 3;
+if (n%3 == 0) {
+  console.log("multiple of 3");
+}
+
+console.log(" ")
+console.log("***");
+console.log("Question 2:")
+console.log(" ")
+
+var a = 18;
+if (a >= 18) {
+  console.log("Apply for Licience");
+}
+else {
+  console.log("NA");
+}
+
+console.log(" ");
+console.log("***");
+console.log("Question 3:");
+console.log(" ");
+
+var b = 4;
+var c = 7;
+if (b>c){
+  console.log("b is greater");
+}
+else if (b<c) {
+  console.log("c is greater");
+}
+if (b==c) {
+  console.log("both are equal")
+}
+
+console.log(" ");
+console.log("***");
+console.log("Question 4:");
+console.log(" ");
+
+var storeuserid = "HelloMoto69";
+var storepass = 8299768422;
+var inputuserid = "HelloMoto";
+var inputpass = 8299768422;
+if (storeuserid==inputuserid) {
+  if (storepass==inputpass) {
+    console.log("login successfull")
+  }
+}
+else {
+  console.log("wrong credentials")
+}
