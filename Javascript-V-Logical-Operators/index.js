8b23d829e3c068fb599d1efd55dae84463f8640d
ANURUDDH <hellomoto.1769@gmail.com>
2022-02-18 14:48:22 +0530

    03/08/2021

---
+++ Javascript-V-Logical-Operators/index.js
@@ -0,0 +1,74 @@
+console.log( "Question1:" );
+console.log( " " );
+var yob = 1994;
+var age = 2021-yob;
+console.log(age);
+if ((age >= 13) && (age <= 19)) {
+console.log("Teenage");
+} else if (( age >= 20) && (age <= 29)) {
+console.log("Twenties");
+}
+console.log( " " );
+console.log( " " );
+console.log( "***" );
+console.log( " " );
+console.log( " " );
+console.log( "Question2:" );
+console.log( " " );
+var char = "a";
+if ((char == "a") || (char == "e") || (char == "i") || (char == "o")  ||  (char == "u") ) {
+console.log("Vowel");
+}
+console.log( " " );
+console.log( " " );
+console.log( "***" );
+console.log( " " );
+console.log( " " );
+console.log( "Question3:" );
+console.log( " " );
+var char = "c";
+if ((char != "a") && (char != "e") && (char != "i") && (char != "o")  &&  (char != "u") ) {
+console.log("Consonant");
+}
+console.log( " " );
+console.log( " " );
+console.log( "***" );
+console.log( " " );
+console.log( " " );
+console.log( "Question4:" );
+console.log( " " );
+var a = 13;
+var b = 5;
+var c = 10;
+if ((a>b) && (a>c)) {
+  console.log("A is greater");
+} else if ((b>a) && (b>c)) {
+  console.log("B is greater");
+} else {
+  console.log("C is greater");
+}
+console.log( " " );
+console.log( " " );
+console.log( "***" );
+console.log( " " );
+console.log( " " );
+console.log( "Question5:" );
+console.log( " " );
+var week = "mon";
+if (week == "sun"){
+  console.log("Sunday");
+}
+else if (week == "mon"){
+  console.log("Monday");
+}
+else if (week == "tue"){
+  console.log("Tuesday");
+}
+else if (week == "wed"){
+  console.log("Wednesday");
+}
+else if (week == "thr"){
+  console.log("Thursday");
+}
+
+console.log("-------------------");
---
+++ Javascript-V-Logical-Operators/saf.js
@@ -0,0 +1,26 @@
+var day = 6
+switch (day) {
+  case 0:
+    console.log("Sunday");
+    break;
+  case 1:
+    console.log("Monday");
+    break;
+  case 2:
+    console.log("Tuesday");
+    break;
+  case 3:
+    console.log("Wednesday");
+    break;
+  case 4:
+    console.log("Thursday");
+    break;
+  case 5:
+    console.log("Friday");
+    break;
+  case 6:
+    console.log("Saturday");
+    break;
+  default:
+    console.log("Invalid Input");
+}
